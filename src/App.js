551240a6601cc24ae6d9df97c2f1642cbb339ca2
Ananta Romadhan <80764638+AnantaR07@users.noreply.github.com>
2023-11-27 15:55:28 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import FooTer from "./components/newFooter";
 import ListProfileGallery from "./listProfileGallery/listProfileGallery";
 import OrderList from "./orderList/orderList";
 import Payment from "./payment/payment";
+import Profile from "./profile/profile";
+import Login from "./loginSignup/Login-Signup-Page";
 
 function App() {
   return (
@@ -12,12 +14,14 @@ function App() {
       <div className="App">
         <NavBar />
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route
             path="/list-profile-gallery"
             element={<ListProfileGallery />}
           />
           <Route path="/order-list" element={<OrderList />} />
           <Route path="/payment" element={<Payment />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
         <FooTer />
       </div>
